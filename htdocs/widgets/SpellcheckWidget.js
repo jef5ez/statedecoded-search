12eba88135369e99b6081748b9c5605dc7802d27
David Dodge <se5zeziroth@gmail.com>
2012-06-07 09:18:24 -0600

    Added spellcheck widget

---
+++ htdocs/widgets/SpellcheckWidget.js
@@ -0,0 +1,18 @@
+(function ($) {
+	AjaxSolr.SpellcheckWidget = AjaxSolr.AbstractWidget.extend({
+
+		
+		afterRequest: function () {
+		  	$(this.target).empty();
+			if (this.manager.response.spellcheck == undefined)
+			{
+				return;
+			}
+			var query = this.manager.store.get("q").value;
+		  	for (var i = 0, l = this.manager.response.spellcheck.suggestions[query].suggestion.length; i < l; i++) {
+				var doc = this.manager.response.spellcheck.suggestions[query].suggestion[i];
+				$(this.target).append(doc, "<p>");
+			}
+		}
+	});
+})(jQuery);
